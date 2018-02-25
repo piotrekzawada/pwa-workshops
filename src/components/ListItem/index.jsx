9d7097d6c69c49f61800d4b7af760458c8360463
piotrekzawada <p.zawada.94@gmail.com>
2018-02-25 12:12:43 +0100

    lazy load

--- src/components/ListItem/index.jsx
+++ src/components/ListItem/index.jsx
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import { string, number } from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, ReactDOM } from 'react-router-dom';
+import LazyLoad from 'react-lazyload';
 import ListRating from '../ListRating';
 import Picture from '../Picture';
 import BottleSvg from '../../svg-components/Bottle';
@@ -9,47 +10,49 @@ import './style.scss';
 class ListItem extends PureComponent {
   render() {
     return (
-      <li>
-        <div className="ListItem">
-          <article>
-            <section className="ListItem__main">
-              <div className="ListItem__photo-container">
-                {this.props.image_url ? (
-                  <Picture className="ListItem__photo" src={this.props.image_url} alt={this.props.name} />
-                ) : (
-                  <BottleSvg className="ListItem__photo" />
-                )}
-              </div>
+        <LazyLoad height={200} overflow>
+          <li>
+            <div className="ListItem">
+              <article>
+                <section className="ListItem__main">
+                  <div className="ListItem__photo-container">
+                      {this.props.image_url ? (
+                          <Picture className="ListItem__photo" src={this.props.image_url} alt={this.props.name} />
+                      ) : (
+                          <BottleSvg className="ListItem__photo" />
+                      )}
+                  </div>
 
-              <div className="ListItem__description">
-                <h2 className="ListItem__annotation ListItem__annotation--small">{this.props.kind}</h2>
+                  <div className="ListItem__description">
+                    <h2 className="ListItem__annotation ListItem__annotation--small">{this.props.kind}</h2>
 
-                <Link to={`/wine/${this.props.id}`}>
-                  <h1 className="ListItem__title">{this.props.name}</h1>
-                </Link>
-                <p className="ListItem__annotation">
-                  {this.props.country_code && (
-                    <img
-                      className="ListItem__icon"
-                      src={`images/country-flags/${this.props.country_code}.svg`}
-                      alt=""
-                    />
-                  )}
-                  <span>{this.props.region}</span>
-                </p>
-              </div>
-            </section>
+                    <Link to={`/wine/${this.props.id}`}>
+                      <h1 className="ListItem__title">{this.props.name}</h1>
+                    </Link>
+                    <p className="ListItem__annotation">
+                        {this.props.country_code && (
+                            <img
+                                className="ListItem__icon"
+                                src={`images/country-flags/${this.props.country_code}.svg`}
+                                alt=""
+                            />
+                        )}
+                      <span>{this.props.region}</span>
+                    </p>
+                  </div>
+                </section>
 
-            <footer className="ListItem__footer">
-              <ListRating className="ListItem__footer-item" rating={this.props.rating} votesCount={this.props.nr_of_ratings} />
+                <footer className="ListItem__footer">
+                  <ListRating className="ListItem__footer-item" rating={this.props.rating} votesCount={this.props.nr_of_ratings} />
 
-              <Link to={`/wine/${this.props.id}`} className="ListItem__footer-item ListItem__footer-action">
-                buy (${this.props.price})
-              </Link>
-            </footer>
-          </article>
-        </div>
-      </li>
+                  <Link to={`/wine/${this.props.id}`} className="ListItem__footer-item ListItem__footer-action">
+                    buy (${this.props.price})
+                  </Link>
+                </footer>
+              </article>
+            </div>
+          </li>
+        </LazyLoad>
     );
   }
 }
